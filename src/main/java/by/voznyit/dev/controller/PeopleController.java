cbfffcfd020f0fccbec034d1e62b3707966eb9bf
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-07-16 16:09:35 +0300

    start to create CRUD DAO app

---
+++ src/main/java/by/voznyit/dev/controller/PeopleController.java
@@ -0,0 +1,17 @@
+package by.voznyit.dev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    @GetMapping()
+    public String index(){
+
+        return null;
+    }
+
+}
