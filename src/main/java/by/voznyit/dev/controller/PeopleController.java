ce42637470feeb394b04fdb2518c6a6101df4d32
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-07-16 16:43:23 +0300

    added links for all users and pages

--- src/main/java/by/voznyit/dev/controller/PeopleController.java
+++ src/main/java/by/voznyit/dev/controller/PeopleController.java
@@ -1,17 +1,43 @@
 package by.voznyit.dev.controller;
 
+import by.voznyit.dev.dao.PersonDAO;
+import by.voznyit.dev.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
-    @GetMapping()
-    public String index(){
 
-        return null;
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO){
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        model.addAttribute("people", personDAO.index());
+        /*List<Person> personList = personDAO.index();
+        for(int i=0;i<personList.size();i++){
+            System.out.println(personList.get(i).getId());
+            System.out.println(personList.get(i).getName());
+        }*/
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model){
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
     }
 
 }
---
+++ src/main/java/by/voznyit/dev/dao/PersonDAO.java
@@ -0,0 +1,34 @@
+package by.voznyit.dev.dao;
+
+import by.voznyit.dev.model.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int peopleCount = 0;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++peopleCount, "Uliana"));
+        people.add(new Person(++peopleCount, "Yauheni"));
+        people.add(new Person(++peopleCount, "Uladzimir"));
+        people.add(new Person(++peopleCount, "Vladislav"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        Person person;
+        for (int i = 0; i < people.size(); i++) {
+            if (people.get(i).getId() == id)
+            return people.get(i);
+        }
+        return null;
+    }
+}
